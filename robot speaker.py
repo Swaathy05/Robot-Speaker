043dca0228a3f2d5733ac3ef29ab3c7af43114bc
Swaathy05 <115560161+Swaathy05@users.noreply.github.com>
2024-06-19 17:06:23 +0530

    Add files via upload

---
+++ robot speaker.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker 1.1. Created by Swaathy")
+    engine = pyttsx3.init()
+    
+    while True:
+        x = input("Enter what you want me to speak (or 'q' to quit): ")
+        if x.lower() == "q":
+            print("Exiting RoboSpeaker. Goodbye!")
+            break
+        engine.say(x)
+        engine.runAndWait()
